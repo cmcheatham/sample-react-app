c61000cdf71e95e16f703fa31c71196f632e174b
Caleb Cheatham <cmc0113@live.com>
2016-11-09 16:24:10 -0600

    Routing Angular 1 App with node app.

--- ui/routes.js
+++ ui/routes.js
@@ -1,24 +1,17 @@
-//Angular 1 Routes
-// customers.routes.js
+// route-config.js
 angular
   .module('client')
-  .run(appRun);
+  .config(config);
 
-/* @ngInject */
-function appRun(routerHelper) {
-  routerHelper.configureStates(getStates());
+function config($stateProvider, $urlRouterProvider, $sceDelegateProvider) {
+  $stateProvider
+  // STATE FOR LOGIN
+    .state('home', {
+      url: '/home',
+      templateUrl: "/home.html",
+      controller: 'HomeCtrl as homeCtrl'
+    })
+  $urlRouterProvider.otherwise('/home');
+
 }
 
-function getStates() {
-  return [
-    {
-      state: 'home',
-      config: {
-        abstract: true,
-        template: 'home.html',
-        url: '/home',
-        controller: 'HomeCtrl'
-      }
-    }
-  ];
-}
