73763261792a036f2f29bd1258e5d98993d67fdd
Caleb Cheatham <cmc0113@live.com>
2016-11-08 19:57:18 -0600

    Adding in routing to home index.html page intended for Angular website.

--- myApp.js
+++ myApp.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var rootViewPath = __dirname + '/ui';
 
 app.get('/', function (req, res) {
   res.send('Root');
 })
 
+app.get('/home', function(req, res, next){
+    res.sendFile(path.join(rootViewPath, 'index.html'));
+});
+
 app.listen(process.env.PORT || 5000, function () {
-  console.log('Example app listening on port 3001!');
+  console.log('Example app listening on port 5000!');
 })
 
